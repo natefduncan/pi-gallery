6693eb6ed37d8061e34eddee434316c1603df9c7
Nathan Duncan <natefduncan@gmail.com>
2021-03-31 23:26:21 -0500

    debug

--- nextcloud.py
+++ nextcloud.py
@@ -7,4 +7,6 @@ from config import PHOTO_DIR
 
 def get_random_photo_path():
     random_file = random.choice(glob.glob(PHOTO_DIR + "/*.jpg", recursive=True))
+    print(f"Selected Photo: {random_file}")
+    print(os.path.join(PHOTO_DIR, random_file))
     return os.path.join(PHOTO_DIR, random_file)
