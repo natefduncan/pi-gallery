a4f834e169c5ce063d3ed22d594b69c0adfbcadb
Nathan Duncan <natefduncan@gmail.com>
2021-04-02 00:15:15 -0500

    edits

--- gallery.py
+++ gallery.py
@@ -15,61 +15,7 @@ def get_image():
         return img
     else:
         return None
-
-class HiddenRoot(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        #hackish way, essentially makes root window
-        #as small as possible but still "focused"
-        #enabling us to use the binding on <esc>
-        self.w, self.h = self.winfo_screenwidth(), self.winfo_screenheight()
-        self.geometry("%dx%d+0+0" % (self.w, self.h))
-
-        self.window = MySlideShow(self)
-        self.window.startSlideShow()
-
-
-class MySlideShow(tk.Toplevel):
-    def __init__(self, *args, **kwargs):
-        tk.Toplevel.__init__(self, *args, **kwargs)
-        #remove window decorations 
-        self.overrideredirect(True)
-        self.w, self.h = self.winfo_screenwidth(), self.winfo_screenheight()
-
-        #save reference to photo so that garbage collection
-        #does not clear image variable in show_image()
-        self.persistent_image = None
-        self.imageList = []
-        self.pixNum = 0
-
-        #used to display as background image
-        self.label = tk.Label(self)
-        self.label.pack(side="top", fill="both", expand=True)
-
-    def startSlideShow(self, delay=10): #delay in seconds
-        img = get_image()
-        img = img.rotate(270, Image.NEAREST, expand = 1)
-        self.showImage(img)
-        #its like a callback function after n seconds (cycle through pics)
-        self.after(delay*1000, self.startSlideShow)
-
-    def showImage(self, image):
-        #Canvas
-        self.focus_set()
-        canvas = tk.Canvas(self, width=self.w, height=self.h)
-        canvas.pack()
-        canvas.configure(background='black')
         
-        # create new image 
-        imgWidth, imgHeight = image.size
-        if imgWidth > self.w or imgHeight > self.h:
-            ratio = min(self.w/imgWidth, self.h/imgHeight)
-            imgWidth = int(imgWidth*ratio)
-            imgHeight = int(imgHeight*ratio)
-            pilImage = image.resize((imgWidth,imgHeight), Image.ANTIALIAS)
-        image = ImageTk.PhotoImage(image)
-        imagesprite = canvas.create_image(self.w/2,self.h/2,image=image)
-
 def PIL_to_canvas(pilImage):
     imgWidth, imgHeight = pilImage.size
     if imgWidth > w or imgHeight > h:
@@ -88,9 +34,10 @@ def image_loop(delay=1):
         img = img.rotate(270, Image.NEAREST, expand = 1)
         print("PIL to canvas")
         PIL_to_canvas(img)
-        time.sleep(5)
-        print("After")
-        canvas.after(0, image_loop)
+        print("Canvas wait")
+        canvas.after(delay*1000)
+        print("Reloop")
+        image_loop()
     except KeyboardInterrupt: 
         quit()
 
@@ -108,9 +55,3 @@ if __name__=="__main__":
 
     image_loop()
     root.mainloop()
-
-    '''
-    slideShow = HiddenRoot()
-    slideShow.bind("<Escape>", lambda e: slideShow.destroy())  # exit on esc
-    slideShow.mainloop()
-    '''
