2ba61ff5bfade2dc4d8b470b28b296828b5b215d
Nathan Duncan <natefduncan@gmail.com>
2021-04-04 13:12:25 -0500

    edits

--- gallery.sh
+++ gallery.sh
@@ -5,14 +5,14 @@ SERVER_PORT=8888
 DELAY=5
 
 #Start slideshow
-sudo fbi -a -noverbose -T 1 -t ${DELAY} --cachemem 0 temp.jpg &&
 
 #Update temp.jpg file on loop
 trap break INT
 while :
     do
-        curl -o temp.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo
+        sudo fbi -a -noverbose -T 1 --cachemem 0 temp.jpg
         sleep ${DELAY}
+        curl -o temp.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo
     done
 trap - INT
 
