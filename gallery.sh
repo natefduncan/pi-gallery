42bedfa80dc85debf6eb62b9a64ff425e6ea6a7b
Nathan Duncan <natefduncan@gmail.com>
2021-04-04 12:59:41 -0500

    edits

--- gallery.sh
+++ gallery.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#Read in vars from .env
-set -o allexport
-source .env
-set +o allexport
+SERVER_ADDRESS="100.68.53.128"
+SERVER_PORT=8888
+DELAY=5
 
 #Start slideshow
 fbi -a -T 1 static/tmp.jpg -noverbose -T 1 -t ${DELAY} --cachemem 0 temp &
