6460fb5fe2dce515a5add278d8d6b96e53dfd457
Nathan Duncan <natefduncan@gmail.com>
2021-04-11 22:06:51 -0500

    photos

--- gallery.sh
+++ gallery.sh
@@ -4,17 +4,26 @@ SERVER_ADDRESS="100.68.53.128"
 SERVER_PORT=8888
 DELAY=5
 
-#Start slideshow
+#Check photos folder has enough files
+photo_count=ls -l | grep -v ^d | wc -l
+echo ${photo_count}
+download_photos=$(( 5 - photo_count ))
+echo ${download_photos}
 
-#Update temp.jpg file on loop
+for photo in $( seq 1 $download_photos )
+do
+    curl -o ./photos/temp${photo}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo 
+done
+
+#Start slideshow
 trap break INT
-sudo fbi -a -noverbose -T 1 -t ${DELAY} --cachemem 0 photos/ & 
-sleep ${DELAY}
+sudo fbi -a -noverbose -T 1 -t ${DELAY} --cachemem 0 ./photos & #Start FBI
+sleep ${DELAY} #Sleep so it can move to next photo before trying to overwrite. 
 while :
-    for counter in {1..10}
+    for counter in {1..5}
         do
-            curl -o /temp${counter}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo && 
-            sleep ${DELAY}
+            curl -o /temp${counter}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo && #Download new photo
+            sleep ${DELAY} 
         done
 trap - INT
 
