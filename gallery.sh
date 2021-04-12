4b6ed5c571be0841187482d72d5aba8701df87bf
Nathan Duncan <natefduncan@gmail.com>
2021-04-11 22:14:55 -0500

    edits

--- gallery.sh
+++ gallery.sh
@@ -10,17 +10,18 @@ download_photos=$(( 5 - photo_count ))
 
 for photo in $( seq 1 $download_photos )
 do
-    curl -o ./photos/temp${photo}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo 
+    curl -o ./photos/temp${photo}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo > /dev/null
+    echo "Adding temp${photo}.jpg"
 done
 
 #Start slideshow
 trap break INT
-sudo fbi -a -noverbose -T 1 -t ${DELAY} --cachemem 0 ./photos & #Start FBI
+sudo fbi -a -noverbose -T 1 -t ${DELAY} --cachemem 0 photos/*jpg & #Start FBI
 sleep ${DELAY} #Sleep so it can move to next photo before trying to overwrite. 
 while :
     for counter in {1..5}
         do
-            curl -o ./photos/temp${counter}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo && \ #Download new photo
+            curl -o ./photos/temp${counter}.jpg http://${SERVER_ADDRESS}:${SERVER_PORT}/random-photo > /dev/null && \ #Download new photo
             sleep ${DELAY} 
         done
 trap - INT
