ab0695607a34732f19b353a3f70bd74e18c447f4
Nathan Duncan <natefduncan@gmail.com>
2021-04-13 20:15:03 -0500

    edits

--- nextcloud.py
+++ nextcloud.py
@@ -8,12 +8,12 @@ from PIL import Image
 from rotate import autorotate
 
 load_dotenv()
-
+PHOTO_DIR = os.getenv("PHOTO_DIR")
 
 
 def get_random_photo_path():
     random_file = random.choice(
-        glob.glob(os.getenv("PHOTO_DIR") + "/*.jpg", recursive=True)
+        glob.glob(PHOTO_DIR + "/*.jpg", recursive=True)
     )
     print(f"Selected Photo: {random_file}")
     print(os.path.join(PHOTO_DIR, random_file))
