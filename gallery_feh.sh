65e9e05a6ac3f83bab18279adf838fdaa31ff15e
natefduncan <natefduncan@gmail.com>
2021-04-13 19:56:12 -0500

    working feh

--- gallery_feh.sh
+++ gallery_feh.sh
@@ -4,4 +4,4 @@ SERVER_ADDRESS="100.68.53.128"
 SERVER_PORT=8888
 DELAY=5
 
-feh -x -F -Y -Z -z -R $DELAY --auto-rotate https://$SERVER_ADDRESS:$SERVER_PORT/random-photo
+feh -x -F -Y -Z -z -R $DELAY --auto-rotate http://$SERVER_ADDRESS:$SERVER_PORT/random-photo
